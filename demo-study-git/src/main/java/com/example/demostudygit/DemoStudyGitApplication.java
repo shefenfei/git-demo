c59f26e7f22ca366593579c540d77eb7074bbe18
shefenfei <aaronshe@shefenfeideiMac.local>
2022-02-20 16:16:40 +0800

    [init] 初始化代码

---
+++ demo-study-git/src/main/java/com/example/demostudygit/DemoStudyGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.demostudygit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoStudyGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoStudyGitApplication.class, args);
+    }
+
+}
