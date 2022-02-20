96e4699ccbe387b42e3a1b582faa39448120599b
shefenfei <she1990111@sina.com>
2022-02-20 16:34:01 +0800

    功能点在10月19号上线

---
+++ demo-study-git/src/main/java/com/example/demostudygit/controller/SayHelloController.java
@@ -0,0 +1,11 @@
+package com.example.demostudygit.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/say")
+public class SayHelloController {
+
+}
